d54c6dd79fd545fe7a77e50fa2120bbefa37404e
Sharon <sharonelizalex@gmail.com>
2021-07-28 10:21:07 +1000

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const {AppoloServer} = require('appolo-server-express');
+const { typeDefs, resolvers } = require ('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new AppoloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({app})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +24,11 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log('use graphql at http://localhost:${PORT}${server.graphqlPath}')
 });
