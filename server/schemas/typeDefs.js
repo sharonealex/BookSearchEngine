7ac3b5540a1126ace623005759c5e95bca11aef7
Sharon <sharonelizalex@gmail.com>
2021-07-28 11:22:38 +1000

    added type def schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const {gql} = require ('appolo-server-express');
+
+const typeDefs = gql `
+type Auth {
+    token: ID!
+    user: User
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+  
+  type Query {
+    me: User
+  }  
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+  `;
+
+  module.exports = typeDefs;
+
+
+
+`
